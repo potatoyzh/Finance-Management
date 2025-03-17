3a283791112da13cc35eea8d8bd4136370a1e5cb
potatoyzh <94159803+potatoyzh@users.noreply.github.com>
2025-03-17 21:29:31 +0000

    Update apps.py

--- finance/apps.py
+++ finance/apps.py
@@ -1,25 +1,14 @@
-from django.apps import AppConfig
-from django.db.utils import OperationalError, ProgrammingError
-from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from finance.models import Admin
 
-class FinanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finance'
+def create_admin_user(request):
+    if not Admin.objects.filter(username='admin').exists():
+        Admin.objects.create(
+            username='admin123',
+            email='admin123@example.com',
+            password='AdminPassword123'
+        )
+        return JsonResponse({'message': 'Custom Admin user created successfully!'})
+    else:
+        return JsonResponse({'message': 'Custom Admin user already exists.'})
 
-    def ready(self):
-        from finance.models import Admin  # 你的自定义 Admin 模型
-
-        try:
-            admin_email = 'admin@example.com'
-            if not Admin.objects.filter(email=admin_email).exists():
-                Admin.objects.create(
-                    username='admin',
-                    email=admin_email,
-                    password='AdminPassword123'
-                )
-                print("✅ Default custom admin user created successfully.")
-            else:
-                print("⚠️ Custom admin user already exists. Skipping creation.")
-        except (OperationalError, ProgrammingError):
-            # 数据库还没迁移时避免报错
-            pass
