4f6d7cb7fadc34c1a08c89fa8ddf72c611918ec8
potatoyzh <94159803+potatoyzh@users.noreply.github.com>
2025-03-17 21:32:47 +0000

    Update apps.py

--- finance/apps.py
+++ finance/apps.py
@@ -1,14 +1,21 @@
 from django.http import JsonResponse
-from finance.models import Admin
+import django.db.utils  
+from django.core.exceptions import ObjectDoesNotExist
 
 def create_admin_user(request):
-    if not Admin.objects.filter(username='admin').exists():
-        Admin.objects.create(
-            username='admin123',
-            email='admin123@example.com',
-            password='AdminPassword123'
-        )
-        return JsonResponse({'message': 'Custom Admin user created successfully!'})
-    else:
-        return JsonResponse({'message': 'Custom Admin user already exists.'})
+    try:
+        from finance.models import Admin
+
+        if not Admin.objects.filter(username='admin123').exists():
+            Admin.objects.create(
+                username='admin123',
+                email='admin123@example.com',
+                password='AdminPassword123'
+            )
+            return JsonResponse({'message': 'Custom Admin user created successfully!'})
+        else:
+            return JsonResponse({'message': 'Custom Admin user already exists.'})
+    
+    except (django.db.utils.OperationalError, django.db.utils.ProgrammingError, ObjectDoesNotExist):
+        return JsonResponse({'error': 'Database not ready. Try again after migrations.'}, status=500)
 
