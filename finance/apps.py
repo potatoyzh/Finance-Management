9cf76a1389a98eff660dd9a5df05d757d23bb264
potatoyzh <94159803+potatoyzh@users.noreply.github.com>
2025-03-17 21:22:40 +0000

    Update apps.py

--- finance/apps.py
+++ finance/apps.py
@@ -1,8 +1,20 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError, ProgrammingError
 
 class FinanceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "finance"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'finance'
+
     def ready(self):
-        import finance.signals  
+        from finance.models import Admin  # 你的自定义 Admin 模型
+
+        try:
+            if not Admin.objects.filter(username='admin').exists():
+                Admin.objects.create(
+                    username='admin',
+                    email='admin@example.com',
+                    password='AdminPassword123'  # 这里要加密
+                )
+                print("Default custom admin user created successfully.")
+        except (OperationalError, ProgrammingError):
+            pass
