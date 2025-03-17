2a84a36206ed2f442e1980efbc65fa56f27f8147
potatoyzh <94159803+potatoyzh@users.noreply.github.com>
2025-03-17 17:14:13 +0000

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -114,7 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 关键：设置 `STATIC_ROOT`
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
